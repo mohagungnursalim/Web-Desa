40c5b49055691f9b8bdd32f753ba3fe45c4ea15f
mohagungnursalim <mohagungnursalim@gmail.com>
2024-10-27 14:18:50 +0800

    fix: menampilkan tampilan highlight teks dan embed video

--- resources/views/livewire/dashboard/posts/form-post.blade.php
+++ resources/views/livewire/dashboard/posts/form-post.blade.php
@@ -156,370 +156,370 @@
             }
     </script>
 
-    <script type="module">
-        import {
-					ClassicEditor,
-					AccessibilityHelp,
-					Alignment,
-					Autoformat,
-					AutoImage,
-					AutoLink,
-					Autosave,
-					Base64UploadAdapter,
-					BlockQuote,
-					BlockToolbar,
-					Bold,
-					CloudServices,
-					Code,
-					CodeBlock,
-					Essentials,
-					FindAndReplace,
-					FontBackgroundColor,
-					FontColor,
-					FontFamily,
-					FontSize,
-					GeneralHtmlSupport,
-					Heading,
-					Highlight,
-					HorizontalLine,
-					ImageBlock,
-					ImageCaption,
-					ImageInline,
-					ImageInsert,
-					ImageInsertViaUrl,
-					ImageResize,
-					ImageStyle,
-					ImageTextAlternative,
-					ImageToolbar,
-					ImageUpload,
-					Indent,
-					IndentBlock,
-					Italic,
-					Link,
-					LinkImage,
-					List,
-					ListProperties,
-					MediaEmbed,
-					Paragraph,
-					RemoveFormat,
-					SelectAll,
-					SourceEditing,
-					SpecialCharacters,
-					SpecialCharactersArrows,
-					SpecialCharactersCurrency,
-					SpecialCharactersEssentials,
-					SpecialCharactersLatin,
-					SpecialCharactersMathematical,
-					SpecialCharactersText,
-					Strikethrough,
-					Style,
-					Subscript,
-					Superscript,
-					Table,
-					TableCaption,
-					TableCellProperties,
-					TableColumnResize,
-					TableProperties,
-					TableToolbar,
-					TextTransformation,
-					TodoList,
-					Underline,
-					Undo
-				} from 'ckeditor5';
-
-				const editorConfig = {
-					toolbar: {
-						items: [
-							'undo',
-							'redo',
-							'|',
-							'sourceEditing',
-							'|',
-							'heading',
-							'style',
-							'|',
-							'fontSize',
-							'fontFamily',
-							'fontColor',
-							'fontBackgroundColor',
-							'|',
-							'bold',
-							'italic',
-							'underline',
-							'|',
-							'link',
-							'insertImage',
-							'insertTable',
-							'highlight',
-							'blockQuote',
-							'codeBlock',
-							'|',
-							'alignment',
-							'|',
-							'bulletedList',
-							'numberedList',
-							'todoList',
-							'outdent',
-							'indent'
-						],
-						shouldNotGroupWhenFull: false
+	<script type="module">
+		import {
+			ClassicEditor,
+			AccessibilityHelp,
+			Alignment,
+			Autoformat,
+			AutoImage,
+			AutoLink,
+			Autosave,
+			Base64UploadAdapter,
+			BlockQuote,
+			BlockToolbar,
+			Bold,
+			CloudServices,
+			Code,
+			CodeBlock,
+			Essentials,
+			FindAndReplace,
+			FontBackgroundColor,
+			FontColor,
+			FontFamily,
+			FontSize,
+			GeneralHtmlSupport,
+			Heading,
+			Highlight,
+			HorizontalLine,
+			ImageBlock,
+			ImageCaption,
+			ImageInline,
+			ImageInsert,
+			ImageInsertViaUrl,
+			ImageResize,
+			ImageStyle,
+			ImageTextAlternative,
+			ImageToolbar,
+			ImageUpload,
+			Indent,
+			IndentBlock,
+			Italic,
+			Link,
+			LinkImage,
+			List,
+			ListProperties,
+			MediaEmbed, // Tambahkan MediaEmbed di sini
+			Paragraph,
+			RemoveFormat,
+			SelectAll,
+			SourceEditing,
+			SpecialCharacters,
+			SpecialCharactersArrows,
+			SpecialCharactersCurrency,
+			SpecialCharactersEssentials,
+			SpecialCharactersLatin,
+			SpecialCharactersMathematical,
+			SpecialCharactersText,
+			Strikethrough,
+			Style,
+			Subscript,
+			Superscript,
+			Table,
+			TableCaption,
+			TableCellProperties,
+			TableColumnResize,
+			TableProperties,
+			TableToolbar,
+			TextTransformation,
+			TodoList,
+			Underline,
+			Undo
+		} from 'ckeditor5';
+	
+		const editorConfig = {
+			toolbar: {
+				items: [
+					'undo',
+					'redo',
+					'|',
+					'sourceEditing',
+					'|',
+					'heading',
+					'style',
+					'|',
+					'fontSize',
+					'fontFamily',
+					'fontColor',
+					'fontBackgroundColor',
+					'|',
+					'bold',
+					'italic',
+					'underline',
+					'|',
+					'link',
+					'insertImage',
+					'insertTable',
+					'highlight',
+					'blockQuote',
+					'codeBlock',
+					'|',
+					'alignment',
+					'|',
+					'bulletedList',
+					'numberedList',
+					'todoList',
+					'outdent',
+					'indent'
+				],
+				shouldNotGroupWhenFull: false
+			},
+			plugins: [
+				AccessibilityHelp,
+				Alignment,
+				Autoformat,
+				AutoImage,
+				AutoLink,
+				Autosave,
+				Base64UploadAdapter,
+				BlockQuote,
+				BlockToolbar,
+				Bold,
+				CloudServices,
+				Code,
+				CodeBlock,
+				Essentials,
+				FindAndReplace,
+				FontBackgroundColor,
+				FontColor,
+				FontFamily,
+				FontSize,
+				GeneralHtmlSupport,
+				Heading,
+				Highlight,
+				HorizontalLine,
+				ImageBlock,
+				ImageCaption,
+				ImageInline,
+				ImageInsert,
+				ImageInsertViaUrl,
+				ImageResize,
+				ImageStyle,
+				ImageTextAlternative,
+				ImageToolbar,
+				ImageUpload,
+				Indent,
+				IndentBlock,
+				Italic,
+				Link,
+				LinkImage,
+				List,
+				ListProperties,
+				MediaEmbed, // Pastikan MediaEmbed ada di sini
+				Paragraph,
+				RemoveFormat,
+				SelectAll,
+				SourceEditing,
+				SpecialCharacters,
+				SpecialCharactersArrows,
+				SpecialCharactersCurrency,
+				SpecialCharactersEssentials,
+				SpecialCharactersLatin,
+				SpecialCharactersMathematical,
+				SpecialCharactersText,
+				Strikethrough,
+				Style,
+				Subscript,
+				Superscript,
+				Table,
+				TableCaption,
+				TableCellProperties,
+				TableColumnResize,
+				TableProperties,
+				TableToolbar,
+				TextTransformation,
+				TodoList,
+				Underline,
+				Undo
+			],
+			blockToolbar: [
+				'fontSize',
+				'fontColor',
+				'fontBackgroundColor',
+				'|',
+				'bold',
+				'italic',
+				'|',
+				'link',
+				'insertImage',
+				'insertTable',
+				'|',
+				'bulletedList',
+				'numberedList',
+				'outdent',
+				'indent'
+			],
+			fontFamily: {
+				supportAllValues: true
+			},
+			fontSize: {
+				options: [10, 12, 14, 'default', 18, 20, 22],
+				supportAllValues: true
+			},
+			heading: {
+				options: [
+					{
+						model: 'paragraph',
+						title: 'Paragraph',
+						class: 'ck-heading_paragraph'
 					},
-					plugins: [
-						AccessibilityHelp,
-						Alignment,
-						Autoformat,
-						AutoImage,
-						AutoLink,
-						Autosave,
-						Base64UploadAdapter,
-						BlockQuote,
-						BlockToolbar,
-						Bold,
-						CloudServices,
-						Code,
-						CodeBlock,
-						Essentials,
-						FindAndReplace,
-						FontBackgroundColor,
-						FontColor,
-						FontFamily,
-						FontSize,
-						GeneralHtmlSupport,
-						Heading,
-						Highlight,
-						HorizontalLine,
-						ImageBlock,
-						ImageCaption,
-						ImageInline,
-						ImageInsert,
-						ImageInsertViaUrl,
-						ImageResize,
-						ImageStyle,
-						ImageTextAlternative,
-						ImageToolbar,
-						ImageUpload,
-						Indent,
-						IndentBlock,
-						Italic,
-						Link,
-						LinkImage,
-						List,
-						ListProperties,
-						MediaEmbed,
-						Paragraph,
-						RemoveFormat,
-						SelectAll,
-						SourceEditing,
-						SpecialCharacters,
-						SpecialCharactersArrows,
-						SpecialCharactersCurrency,
-						SpecialCharactersEssentials,
-						SpecialCharactersLatin,
-						SpecialCharactersMathematical,
-						SpecialCharactersText,
-						Strikethrough,
-						Style,
-						Subscript,
-						Superscript,
-						Table,
-						TableCaption,
-						TableCellProperties,
-						TableColumnResize,
-						TableProperties,
-						TableToolbar,
-						TextTransformation,
-						TodoList,
-						Underline,
-						Undo
-					],
-					blockToolbar: [
-						'fontSize',
-						'fontColor',
-						'fontBackgroundColor',
-						'|',
-						'bold',
-						'italic',
-						'|',
-						'link',
-						'insertImage',
-						'insertTable',
-						'|',
-						'bulletedList',
-						'numberedList',
-						'outdent',
-						'indent'
-					],
-					fontFamily: {
-						supportAllValues: true
+					{
+						model: 'heading1',
+						view: 'h1',
+						title: 'Heading 1',
+						class: 'ck-heading_heading1'
 					},
-					fontSize: {
-						options: [10, 12, 14, 'default', 18, 20, 22],
-						supportAllValues: true
+					{
+						model: 'heading2',
+						view: 'h2',
+						title: 'Heading 2',
+						class: 'ck-heading_heading2'
 					},
-					heading: {
-						options: [
-							{
-								model: 'paragraph',
-								title: 'Paragraph',
-								class: 'ck-heading_paragraph'
-							},
-							{
-								model: 'heading1',
-								view: 'h1',
-								title: 'Heading 1',
-								class: 'ck-heading_heading1'
-							},
-							{
-								model: 'heading2',
-								view: 'h2',
-								title: 'Heading 2',
-								class: 'ck-heading_heading2'
-							},
-							{
-								model: 'heading3',
-								view: 'h3',
-								title: 'Heading 3',
-								class: 'ck-heading_heading3'
-							},
-							{
-								model: 'heading4',
-								view: 'h4',
-								title: 'Heading 4',
-								class: 'ck-heading_heading4'
-							},
-							{
-								model: 'heading5',
-								view: 'h5',
-								title: 'Heading 5',
-								class: 'ck-heading_heading5'
-							},
-							{
-								model: 'heading6',
-								view: 'h6',
-								title: 'Heading 6',
-								class: 'ck-heading_heading6'
-							}
-						]
+					{
+						model: 'heading3',
+						view: 'h3',
+						title: 'Heading 3',
+						class: 'ck-heading_heading3'
 					},
-					htmlSupport: {
-						allow: [
-							{
-								name: /^.*$/,
-								styles: true,
-								attributes: true,
-								classes: true
-							}
-						]
+					{
+						model: 'heading4',
+						view: 'h4',
+						title: 'Heading 4',
+						class: 'ck-heading_heading4'
 					},
-					image: {
-						toolbar: [
-							'toggleImageCaption',
-							'imageTextAlternative',
-							'|',
-							'imageStyle:inline',
-							'imageStyle:wrapText',
-							'imageStyle:breakText',
-							'|',
-							'resizeImage'
-						]
+					{
+						model: 'heading5',
+						view: 'h5',
+						title: 'Heading 5',
+						class: 'ck-heading_heading5'
 					},
-					
-					link: {
-						addTargetToExternalLinks: true,
-						defaultProtocol: 'https://',
-						decorators: {
-							toggleDownloadable: {
-								mode: 'manual',
-								label: 'Downloadable',
-								attributes: {
-									download: 'file'
-								}
-							}
-						}
-					},
-					list: {
-						properties: {
-							styles: true,
-							startIndex: true,
-							reversed: true
-						}
-					},
-					menuBar: {
-						isVisible: true
-					},
-					placeholder: 'Ketik atau paste konten Anda di sini!',
-					style: {
-						definitions: [
-							{
-								name: 'Article category',
-								element: 'h3',
-								classes: ['category']
-							},
-							{
-								name: 'Title',
-								element: 'h2',
-								classes: ['document-title']
-							},
-							{
-								name: 'Subtitle',
-								element: 'h3',
-								classes: ['document-subtitle']
-							},
-							{
-								name: 'Info box',
-								element: 'p',
-								classes: ['info-box']
-							},
-							{
-								name: 'Side quote',
-								element: 'blockquote',
-								classes: ['side-quote']
-							},
-							{
-								name: 'Marker',
-								element: 'span',
-								classes: ['marker']
-							},
-							{
-								name: 'Spoiler',
-								element: 'span',
-								classes: ['spoiler']
-							},
-							{
-								name: 'Code (dark)',
-								element: 'pre',
-								classes: ['fancy-code', 'fancy-code-dark']
-							},
-							{
-								name: 'Code (bright)',
-								element: 'pre',
-								classes: ['fancy-code', 'fancy-code-bright']
-							}
-						]
-					},
-					table: {
-						contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+					{
+						model: 'heading6',
+						view: 'h6',
+						title: 'Heading 6',
+						class: 'ck-heading_heading6'
 					}
-				};
-
-				let debounceTimer;
-
-				ClassicEditor
-					.create(document.querySelector('#editor'), editorConfig)
-					.then(editor => {
-						// Debounce ketika ada perubahan pada editor
-						editor.model.document.on('change:data', () => {
-							clearTimeout(debounceTimer);
-							debounceTimer = setTimeout(() => {
-								@this.set('description', editor.getData()); // Update Livewire property
-							}, 1000); // Debounce selama 1 detik
-						});
-					})
-					.catch(error => {
-						console.error(error);
-					});
-
-    </script>
+				]
+			},
+			htmlSupport: {
+				allow: [
+					{
+						name: /^.*$/,
+						styles: true,
+						attributes: true,
+						classes: true
+					}
+				]
+			},
+			image: {
+				toolbar: [
+					'toggleImageCaption',
+					'imageTextAlternative',
+					'|',
+					'imageStyle:inline',
+					'imageStyle:wrapText',
+					'imageStyle:breakText',
+					'|',
+					'resizeImage'
+				]
+			},
+			link: {
+				addTargetToExternalLinks: true,
+				defaultProtocol: 'https://',
+				decorators: {
+					toggleDownloadable: {
+						mode: 'manual',
+						label: 'Dapat diunduh',
+						attributes: {
+							download: 'file'
+						}
+					}
+				}
+			},
+			list: {
+				properties: {
+					styles: true,
+					startIndex: true,
+					reversed: true
+				}
+			},
+			menuBar: {
+				isVisible: true
+			},
+			placeholder: 'Ketik atau paste konten Anda di sini!',
+			style: {
+				definitions: [
+					{
+						name: 'Article category',
+						element: 'h3',
+						classes: ['category']
+					},
+					{
+						name: 'Title',
+						element: 'h2',
+						classes: ['document-title']
+					},
+					{
+						name: 'Subtitle',
+						element: 'h3',
+						classes: ['document-subtitle']
+					},
+					{
+						name: 'Info box',
+						element: 'p',
+						classes: ['info-box']
+					},
+					{
+						name: 'Side quote',
+						element: 'blockquote',
+						classes: ['side-quote']
+					},
+					{
+						name: 'Marker',
+						element: 'span',
+						classes: ['marker']
+					},
+					{
+						name: 'Spoiler',
+						element: 'span',
+						classes: ['spoiler']
+					},
+					{
+						name: 'Code (dark)',
+						element: 'pre',
+						classes: ['fancy-code', 'fancy-code-dark']
+					},
+					{
+						name: 'Code (bright)',
+						element: 'pre',
+						classes: ['fancy-code', 'fancy-code-bright']
+					}
+				]
+			},
+			table: {
+				contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+			}
+		};
+	
+		let debounceTimer;
+	
+		ClassicEditor
+			.create(document.querySelector('#editor'), editorConfig)
+			.then(editor => {
+				// Debounce ketika ada perubahan pada editor
+				editor.model.document.on('change:data', () => {
+					clearTimeout(debounceTimer);
+					debounceTimer = setTimeout(() => {
+						@this.set('description', editor.getData()); // Update Livewire property
+					}, 1000); // Debounce selama 1 detik
+				});
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	
+	</script>
+	
 
     {{-- Select2 Kategori--}}
     <script>
--- resources/views/livewire/dashboard/posts/post.blade.php
+++ resources/views/livewire/dashboard/posts/post.blade.php
@@ -1,21 +1,59 @@
 <div class="py-4">
     @push('styles')
-   <style>
-    .post-content img {
-        max-width: 100%;
-        height: auto;
-      
-    }
-   </style>
+    <style>
+        /* Gaya untuk blok kode */
+        pre {
+            background-color: #000000;
+            /* Background gelap */
+            color: #f8f8f2;
+            /* Warna teks */
+            padding: 15px;
+            border-radius: 8px;
+            overflow-x: auto;
+            /* Scroll horizontal jika teks terlalu panjang */
+            font-family: 'Courier New', Courier, monospace;
+            /* Font monospaced */
+            font-size: 14px;
+            position: relative;
+        }
+
+    </style>
+
+    <style>
+        .post-content img {
+            max-width: 100%;
+            height: auto;
+
+        }
+
+        /* CSS untuk highlight */
+        .marker-yellow {
+            background-color: #fdfd77;
+        }
+
+        .marker-green {
+            background-color: #63f963;
+        }
+
+        .marker-pink {
+            background-color: #fc92c4;
+        }
+
+        .marker-blue {
+            background-color: #9ecbff;
+        }
+
+    </style>
     @endpush
     <div class="container-fluid col-md">
         <div class="card" style="border-radius: 25px;">
             <div class="card-body" style="padding: 20px;">
-                <!-- Tombol untuk membuka modal -->
-                <a href="/dashboard/postingan/tambah-data" style="border-radius: 10px;" class="btn btn-primary mb-4">Tambah
+                
+                <a href="/dashboard/postingan/tambah-data" style="border-radius: 10px;"
+                    class="btn btn-primary mb-4">Tambah
                     Postingan</a>
 
-                <!-- Input untuk mencari postingan -->
+                
                 <div class="mb-2">
                     <input type="text" wire:model.live.debounce.500ms="search" placeholder="Cari postingan.."
                         class="form-control" style="color: black; border-radius: 10px;">
@@ -59,7 +97,7 @@
                                 </td>
                                 <td>
                                     @foreach ($post->tags as $tag)
-                                        <a>#{{ $tag->name }}</a>
+                                    <a>#{{ $tag->name }}</a>
                                     @endforeach
                                 </td>
                                 <td>{{ $post->excerpt }}</td>
@@ -87,16 +125,16 @@
                                         <i class="bi bi-eye"></i>
                                     </button>
                                     <!-- Tombol edit data -->
-                                    <a href="{{ route('dashboard.post.edit',$post->slug) }}" style="border-radius: 10px;" 
-                                        class="btn btn-warning mb-1 me-2">
-                                        
+                                    <a href="{{ route('dashboard.post.edit',$post->slug) }}"
+                                        style="border-radius: 10px;" class="btn btn-warning mb-1 me-2">
+
                                         <i class="bi bi-pencil-square"></i>
                                     </a>
 
                                     <!-- Tombol untuk membuka modal delete -->
                                     <button style="border-radius: 10px;" data-toggle="modal"
-                                        data-target="#modalDelete{{ $post->id }}"
-                                        type="button" class="btn btn-danger text-white mb-1 me-2">
+                                        data-target="#modalDelete{{ $post->id }}" type="button"
+                                        class="btn btn-danger text-white mb-1 me-2">
                                         <!-- Tambahkan kelas me-2 untuk margin di sebelah kanan -->
                                         <i class="bi bi-trash"></i>
                                     </button>
@@ -183,7 +221,7 @@
     <div class="modal" id="modalDelete{{ $post->id }}" tabindex="-1" role="dialog"
         aria-labelledby="deleteModalLabel{{ $post->id }}" aria-hidden="true">
         <div class="modal-dialog" role="document">
-            <div class="modal-content"  style="border-radius: 20px;">
+            <div class="modal-content" style="border-radius: 20px;">
                 <div class="modal-header">
                     <h6 class="modal-title" id="deleteModalLabel{{ $post->id }}">
                         Hapus Postingan "{{ $post->title }}" </h6>
@@ -195,16 +233,16 @@
                     Apakah anda yakin ingin menghapus postingan ini?
                 </div>
                 <div class="modal-footer justify-content-center">
-                    <button style="border-radius: 10px;"  wire:loading.remove wire:target='delete({{ $post->id }})' type="button"
-                        class="btn btn-secondary" data-dismiss="modal">Batal
+                    <button style="border-radius: 10px;" wire:loading.remove wire:target='delete({{ $post->id }})'
+                        type="button" class="btn btn-secondary" data-dismiss="modal">Batal
                     </button>
-                    <button style="border-radius: 10px;" wire:loading.remove wire:click="delete({{ $post->id }})" type="button"
-                        class="btn btn-danger">Hapus
+                    <button style="border-radius: 10px;" wire:loading.remove wire:click="delete({{ $post->id }})"
+                        type="button" class="btn btn-danger">Hapus
                     </button>
 
-                    <button style="border-radius: 10px;" wire:loading wire:target='delete({{ $post->id }})' class="btn btn-danger" disabled>
-                        Menghapus <span class="spinner-grow spinner-grow-sm" role="status"
-                            aria-hidden="true"></span>
+                    <button style="border-radius: 10px;" wire:loading wire:target='delete({{ $post->id }})'
+                        class="btn btn-danger" disabled>
+                        Menghapus <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
                     </button>
                 </div>
             </div>
@@ -290,22 +328,74 @@
             });
         });
 
-    </script>  
+    </script>
 
-        {{-- Sweet alert,delete success --}}
-        <script>
-            $(document).ready(function () {
-                window.addEventListener('deleteSuccess', function (event) {
-                    Swal.fire({
-                        title: "Sukses!",
-                        text: "Postingan berhasil dihapus!",
-                        icon: "success",
-                        timer: 1000,
-                        timerProgressBar: true,
-                    });
+    {{-- Sweet alert,delete success --}}
+    <script>
+        $(document).ready(function () {
+            window.addEventListener('deleteSuccess', function (event) {
+                Swal.fire({
+                    title: "Sukses!",
+                    text: "Postingan berhasil dihapus!",
+                    icon: "success",
+                    timer: 1000,
+                    timerProgressBar: true,
                 });
-            })
+            });
+        })
+
+    </script>
+
+
+
+
+    {{-- Code Style --}}
+    <script>
+        // Cari semua elemen <pre> di dalam CKEditor
+        $('.post-content pre').each(function () {
+            var pre = $(this);
+
+            // Cek jika header sudah ditambahkan, jika iya, lewati
+            if (pre.prev('.code-header').length > 0) return;
+
+            // Ambil class bahasa dari elemen <code>
+            var languageClass = pre.find('code').attr('class');
+            var languageName = languageClass ? languageClass.split('-')[1] : 'kode';
+
+            // Buat header dengan gaya yang sesuai
+            var header = $(
+                '<div class="code-header" style="background-color: #3d3d3d; color: #f8f8f2; padding: 5px; border-bottom: 1px solid #ccc; font-family: \'Courier New\', Courier, monospace; font-size: 14px; border-radius: 5px; font-weight: bold;">' +
+                languageName + '</div>');
+
+            // Tempatkan header di atas elemen <pre>
+            pre.before(header);
+        });
+
+    </script>
+  
     
-        </script>
+    {{-- Menampilkan Iframe --}}
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            // Temukan semua elemen <oembed> dan konversi ke <iframe>
+            document.querySelectorAll('oembed[url]').forEach(element => {
+                // Ambil URL dari atribut `url`
+                const url = element.getAttribute('url');
+
+                // Buat elemen <iframe>
+                const iframe = document.createElement('iframe');
+                iframe.setAttribute('width', '100%');
+                iframe.setAttribute('height', '400');
+                iframe.setAttribute('frameborder', '0');
+                iframe.setAttribute('allowfullscreen', '');
+                iframe.setAttribute('src', url.replace("watch?v=", "embed/"));
+
+                // Ganti <oembed> dengan <iframe>
+                element.parentNode.replaceChild(iframe, element);
+            });
+        });
+    </script>
+
+
     @endpush
 </div>
