3975e592e1ab95d24aacb5af91a78af869887251
mohagungnursalim <mohagungnursalim@gmail.com>
2024-12-16 10:04:20 +0800

    fix: memasukan script dropdown navbar ke app2.js

--- resources/js/app2.js
+++ resources/js/app2.js
@@ -8,6 +8,22 @@ document.addEventListener('livewire:navigated', () => {
         initFlowbite();
 });
 
+document.addEventListener('livewire:navigated', () => {
+        const dropdownButton = document.getElementById('dropdownButton');
+        const dropdownMenu = document.getElementById('dropdownMenu');
+    
+        // Toggle dropdown visibility
+        dropdownButton.addEventListener('click', () => {
+          dropdownMenu.classList.toggle('hidden');
+        });
+    
+        // Close dropdown if clicked outside
+        document.addEventListener('click', (e) => {
+          if (!dropdownButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
+            dropdownMenu.classList.add('hidden');
+          }
+        });
+});
 
 
 
--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -78,21 +78,3 @@
     </div>
   </nav>
 
-<script>
-  document.addEventListener('DOMContentLoaded', () => {
-    const dropdownButton = document.getElementById('dropdownButton');
-    const dropdownMenu = document.getElementById('dropdownMenu');
-
-    // Toggle dropdown visibility
-    dropdownButton.addEventListener('click', () => {
-      dropdownMenu.classList.toggle('hidden');
-    });
-
-    // Close dropdown if clicked outside
-    document.addEventListener('click', (e) => {
-      if (!dropdownButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
-        dropdownMenu.classList.add('hidden');
-      }
-    });
-  });
-</script>
