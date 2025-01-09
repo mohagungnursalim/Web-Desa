c1849d2ca6a2c960218cc16397d5471d3a964896
mohagungnursalim <mohagungnursalim@gmail.com>
2025-01-09 11:42:58 +0800

    fix: menghilangkan judul post di breadcrumb

--- resources/views/livewire/frontend/berita/berita-detail.blade.php
+++ resources/views/livewire/frontend/berita/berita-detail.blade.php
@@ -2,8 +2,8 @@
  
 <main class="pt-8 pb-16  lg:pb-24 bg-white antialiased">
     
-
-<nav class="flex mb-4" aria-label="Breadcrumb">
+<!-- Breadcrumb -->
+<nav class="flex mb-4 px-5 py-3" aria-label="Breadcrumb">
     <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
      
       <li>
@@ -16,7 +16,7 @@
           <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
           </svg>
-          <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 ">{{ $post->title }}</span>
+
         </div>
       </li>
     </ol>
